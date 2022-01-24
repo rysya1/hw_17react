d23d0529deb8b43c09906ff8535f32fe014d0748
rysya1 <rysya.bokombaev@gmail.com>
2022-01-24 17:38:36 +0600

    coment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,11 +17,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear
   }) //filteredExpenses полный фильтырация болгон массивти сактайт
 
-  let expensesContent = <p style={{textAlign: 'center', color: 'white'}}>No 
-  Expenses Found</p>
 
-  if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((element) => { //бул жакта полный фильтырация болгон массивти аралап атабыз 
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {console.log(filteredYear)}
+      {filteredExpenses.map((element) => { //бул жакта полный фильтырация болгон массивти аралап атабыз 
       return (
         <ExpenseItem
           key={Math.random()} //бул жакта реакт учун уникальный ключ беришибиз керек а то консольдон ошибка чыга берет но если биз баарын озубуз map жок кылып чыксак анда уже анын под копотом озунун ключу создаётся
@@ -29,37 +30,9 @@ const Expenses = (props) => {
           amount={element.amount}
           date={element.date}
         />
-      );
-    })
-  }
-   if (filteredExpenses === '') {
-     
-   }
-  return (
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {console.log(filteredYear)}
-      {}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+        )
+      })}
+  
       {filteredYear === "all" && props.items.map((element) => { //бул жакта полный фильтырация болгон массивти аралап атабыз
       return (
         <ExpenseItem
